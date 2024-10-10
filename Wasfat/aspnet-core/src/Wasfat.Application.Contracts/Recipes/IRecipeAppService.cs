a2b51b4703951729cd3313f9cc2edaf087e159cf
MibAX <Instructors@mibax.com>
2024-10-10 17:05:59 +0200

    Defining the Service Interface

---
+++ Wasfat/aspnet-core/src/Wasfat.Application.Contracts/Recipes/IRecipeAppService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Wasfat.Recipes
+{
+    public interface IRecipeAppService : ICrudAppService<
+             RecipeDto,
+             int,
+             PagedAndSortedResultRequestDto>
+    {
+    }
+}
