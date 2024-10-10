ab6caff2e7d3457a9d6fa970d4c8142c7c4ce73d
MibAX <Instructors@mibax.com>
2024-10-10 17:05:42 +0200

    Setting Up the Mapper Profile

---
+++ Wasfat/aspnet-core/src/Wasfat.Application/Recipes/RecipeMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wasfat.Recipes
+{
+    public class RecipeMapperProfile : Profile
+    {
+        public RecipeMapperProfile()
+        {
+            CreateMap<Recipe, RecipeDto>().ReverseMap();
+        }
+    }
+}
