47f31ee4bf9a4002d3fda1c5411dc0d190d69580
MibAX <Instructors@mibax.com>
2024-10-10 17:06:23 +0200

    Implementing the Service

---
+++ Wasfat/aspnet-core/src/Wasfat.Application/Recipes/RecipeAdminAppService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Wasfat.Recipes
+{
+    public class RecipeAdminAppService : CrudAppService<Recipe, RecipeDto, int, PagedAndSortedResultRequestDto>, IRecipeAppService
+    {
+        public RecipeAdminAppService(
+            IRepository<Recipe, int> repository
+            )
+        : base(repository)
+        {
+                
+        }
+    }
+}
