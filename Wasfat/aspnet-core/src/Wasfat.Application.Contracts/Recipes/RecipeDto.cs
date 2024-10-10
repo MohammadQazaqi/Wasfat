fd8e06afbc8d5fe250bcbf87088bc8d208d6e0ba
MibAX <Instructors@mibax.com>
2024-10-10 17:05:26 +0200

    Creating the Data Transfer Object (DTO)

---
+++ Wasfat/aspnet-core/src/Wasfat.Application.Contracts/Recipes/RecipeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace Wasfat.Recipes
+{
+
+    public class RecipeDto : EntityDto<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
