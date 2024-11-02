6cf4a940582a64b6731fc851b149f2ac44069a0a
MibAX <Instructors@mibax.com>
2024-11-02 21:20:29 +0100

    Adding the Main Route to the Recipes Module

--- Wasfat/admin.angular/src/app/app-routing.module.ts
+++ Wasfat/admin.angular/src/app/app-routing.module.ts
@@ -25,6 +25,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('@abp/ng.setting-management').then(m => m.SettingManagementModule.forLazy()),
   },
+  {
+    path: 'recipes',
+    pathMatch: 'full',
+    loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule),
+  },
 ];
 
 @NgModule({
