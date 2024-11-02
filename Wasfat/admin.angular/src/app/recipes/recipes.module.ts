7d746fb9dfa98f13a516ce4bea5a183beaaa1e34
MibAX <Instructors@mibax.com>
2024-11-02 21:01:46 +0100

    Generating the Recipes List Component

--- Wasfat/admin.angular/src/app/recipes/recipes.module.ts
+++ Wasfat/admin.angular/src/app/recipes/recipes.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RecipesRoutingModule } from './recipes-routing.module';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RecipesListComponent
+  ],
   imports: [
     CommonModule,
     RecipesRoutingModule
