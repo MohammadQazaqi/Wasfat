fc3709618a553c08f21f0230604f2131e404cb3c
MibAX <Instructors@mibax.com>
2024-11-10 18:00:31 +0100

    08.03 - Creating the (Get All Recipes) Endpoint

--- Wasfat/aspnet-core/src/Wasfat.Application/Recipes/RecipeAdminAppService.cs
+++ Wasfat/aspnet-core/src/Wasfat.Application/Recipes/RecipeAdminAppService.cs
@@ -115,6 +115,15 @@ namespace Wasfat.Recipes
             return recentRecipeDtos;
         }
 
+        public async Task<List<RecipeDto>> GetAllRecipesAsync()
+        {
+            var recipes = await _recipesRepository.GetListAsync();
+
+            var recipeDtos = ObjectMapper.Map<List<Recipe>, List<RecipeDto>>(recipes);
+
+            return recipeDtos;
+        }
+
 
 
 
